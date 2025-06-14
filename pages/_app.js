2263b4f87478beb7abb052c8e82581ffde2dfa46
foxcryptoboss <92148851+foxcryptoboss@users.noreply.github.com>
2025-06-14 23:46:11 +0100

    Add pages/_app.js

---
+++ pages/_app.js
@@ -0,0 +1,35 @@
+import React, { useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+
+// Import wallet adapter CSS
+require('@solana/wallet-adapter-react-ui/styles.css');
+
+function MyApp({ Component, pageProps }) {
+  // You can use WalletAdapterNetwork.Mainnet, WalletAdapterNetwork.Testnet, or WalletAdapterNetwork.Devnet
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter(),
+    ],
+    []
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
+
+export default MyApp;
